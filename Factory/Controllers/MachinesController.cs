5160c68c0450fcb40e8acda3c1c728d6c380f740
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-04 12:06:36 -0700

    completed first part of MachinesController. Controllers WIP.

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+    
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(machine => machine.MachineId ==id);
+      return View(thisMachine);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
