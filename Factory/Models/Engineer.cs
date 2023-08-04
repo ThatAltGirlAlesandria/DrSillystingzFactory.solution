4ce00f04e8d21637097259c86bdfa36ba4fb764d
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-04 11:37:01 -0700

    Finsished models.

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models;
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+
+    [Required(ErrorMessage = "You must enter the name of the Engineer.")]
+    public string EngineerName { get; set; }
+
+    [Required(ErrorMessage = "You cannot leave the Engineer's details blank.")]
+    public string EngineerDetails { get; set; }
+
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotation;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "You must enter the name of the Machine")]
+    public string MachineName { get; set; }
+
+    [Required(ErrorMessage = "You cannot leave the Machine's details blank.")]
+    public string MachineDescription { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
